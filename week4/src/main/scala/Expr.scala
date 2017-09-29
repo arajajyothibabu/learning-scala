720780ff8705242f2c314475265731b9615ee704
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-09-29 18:50:03 +0530

    week4 class exercise

---
+++ week4/src/main/scala/Expr.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by jyothi on 27/9/17.
+  */
+
+trait Expr
+
+case class Number(n: Int) extends Expr
+
+case class Sum(e1: Expr, e2: Expr) extends Expr
+
+case class Prod(e1: Expr, e2: Expr) extends Expr
+
+case class Var(x: String) extends Expr
