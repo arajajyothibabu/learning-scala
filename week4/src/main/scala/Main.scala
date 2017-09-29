955c2bfe8bc3f4adddbae6108a93bafc96a4a8ef
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-09-29 18:50:31 +0530

    week4 assignment

--- week4/src/main/scala/Main.scala
+++ week4/src/main/scala/Main.scala
@@ -3,4 +3,17 @@
   */
 object Main extends App {
 
+  def show(e: Expr): String = e match {
+    case Number(e) => e.toString
+    case Sum(e1, e2) => s"${show(e1)} + ${show(e2)}"
+  }
+
+  //println(show(Sum(Number(1), Number(2))))
+
+  import scala.collection.immutable.List
+  def insert(x: Int, xs: scala.collection.immutable.List[Int]): scala.collection.immutable.List[Int] = xs match {
+    case List() => List(x)
+    case y :: ys => if(x <= y) x :: xs else y :: insert(x, ys)
+  }
+
 }
